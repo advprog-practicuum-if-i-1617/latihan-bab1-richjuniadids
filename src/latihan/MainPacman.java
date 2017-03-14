8b4914acdf64d1392bc7436b521f2c5577abe9bc
richjuniadids <richjuniadi2@gmail.com>
2017-03-14 22:28:51 +0700

    Update and rename Main.java to MainPacman.java

---
+++ src/latihan/MainPacman.java
@@ -0,0 +1,27 @@
+package latihan;
+
+/**
+ *
+ * @author richjuniadi
+ */
+import java.util.Scanner;
+public class MainPacman {
+public static void main(String[] args) 
+    {
+        Pacman pacman1 = new Pacman ();
+        Scanner a = new Scanner (System.in);
+        String pilihan="m";
+        while (pilihan.equals("m")){
+            System.out.print("Masukkan m untuk memulai k keluar : ");
+            pilihan = a.next();
+            if (pilihan.equals("m"))
+            {
+                pacman1.cekposisi();
+                pacman1.karakter();
+                pacman1.shift();
+                pacman1.displayMessage();
+                pacman1.poinLangkah();
+               }
+            }
+        }
+    }    
